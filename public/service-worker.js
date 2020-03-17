2ac09a57f1b08bc8dd369f40fd7a083aa3dad0bd
ManoukK <manouk.kappe21@gmail.com>
2020-03-17 18:04:12 +0100

    service worker bug fix
    ik kan nu offline een berichtje laten zien (dankzij Robin Frugte)

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,36 +1,23 @@
-self.addEventListener('install', function(event){
+self.addEventListener('install', function (event) {
     console.log('installing');
     self.skipWaiting();
 });
 
-self.addEventListener('activate', function(event){
+self.addEventListener('activate', function (event) {
     console.log('activating!');
-    event.waitUnil(clients.claim());
+    event.waitUntil(clients.claim());
 });
 
-// self.addEventListener('fetch', function(event){
-//     if (isHtmlGetRequest(event.request)) {
-//         console.log('html get request', event.request.url)
-//         event.respondWith(
-//             fetch(event.request).catch(function(e){
-//                 return new Response('je bent offline!');
-//             })
-//         )
-//     }
-// });
-
 self.addEventListener('fetch', function(event){
-    if (isHtmlGetRequest(event.request)) {
+
         console.log('html get request', event.request.url)
         event.respondWith(
-            fetch(event.request)
-        )
-    }
-}).catch(function(e){
+            fetch(event.request).catch(function(e){
                 return new Response('je bent offline!');
-            }); 
+            })
+        )
+});
 
-//checkt of de request een html en een get request is.
-function isHtmlGetRequest(request){
+function isHtmlGetRequest(request) {
     return request.method === 'GET' && (request.headers.get('accept') !== null && request.headers.get('accept'.indexOf('text/html') > -1));
 }
